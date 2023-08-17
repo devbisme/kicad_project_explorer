5f40a5aa9bcced1cf9e36236147c5f6e00225553
devbisme <devb@xess.com>
2023-08-17 08:58:29 -0400

    feat: Added primitive vanilla-JS web interface to scroll thorugh KiCad project JSON file.

---
+++ web_interface/app.js
@@ -0,0 +1,105 @@
+let jsonData = [];
+let sortDirection = {};
+
+window.onload = function() {
+    // fetch('https://example.com/data.json')
+    fetch('/kicad_repos.json')
+        .then(response => response.json())
+        .then(data => {
+            jsonData = data;
+            populateTable(data);
+        });
+}
+
+// function populateTable(data) {
+//     const table = document.getElementById('dataTable');
+//     table.innerHTML = '';
+//     let thead = document.createElement('thead');
+//     let tbody = document.createElement('tbody');
+
+//     let keys = Object.keys(data[0]);
+//     let headerRow = document.createElement('tr');
+//     keys.forEach(key => {
+//         let th = document.createElement('th');
+//         th.onclick = function() { sortTable(key); };
+//         th.textContent = key;
+//         th.innerHTML += sortDirection[key] === 'asc' ? ' &#9650;' : ' &#9660;';
+//         headerRow.appendChild(th);
+//     });
+//     thead.appendChild(headerRow);
+
+//     data.forEach((object, index) => {
+//         let tr = document.createElement('tr');
+//         tr.className = index % 2 === 0 ? 'even-row' : 'odd-row';
+//         keys.forEach(key => {
+//             let td = document.createElement('td');
+//             td.textContent = object[key];
+//             tr.appendChild(td);
+//         });
+//         tbody.appendChild(tr);
+//     });
+
+//     table.appendChild(thead);
+//     table.appendChild(tbody);
+// }
+
+function populateTable(data) {
+    const table = document.getElementById('dataTable');
+    table.innerHTML = '';
+    let thead = document.createElement('thead');
+    let tbody = document.createElement('tbody');
+
+    let keys = Object.keys(data[0]);
+    let headerRow = document.createElement('tr');
+    keys.forEach(key => {
+        let th = document.createElement('th');
+        th.onclick = function() { sortTable(key); };
+        
+        let div = document.createElement('div');
+        // div.className = 'cell';
+        div.className = 'cell.' + key;
+        // div.textContent = key + (sortDirection[key] === 'asc' ? ' ▲' : ' ▼');
+        // div.textContent = key + (sortDirection[key] === 'asc' ? ' &#9650;' : ' &#9660;');
+        div.innerHTML = key + (sortDirection[key] === 'asc' ? ' &#9650;' : ' &#9660;');
+        
+        th.appendChild(div);
+        headerRow.appendChild(th);
+    });
+    thead.appendChild(headerRow);
+
+    data.forEach((object, index) => {
+        let tr = document.createElement('tr');
+        tr.className = index % 2 === 0 ? 'even-row' : 'odd-row';
+        keys.forEach(key => {
+            let td = document.createElement('td');
+            
+            let div = document.createElement('div');
+            div.className = 'cell';
+            let div2 = document.createElement('div');
+            div2.className = key;
+            div2.textContent = object[key];
+            
+            div.appendChild(div2)
+            td.appendChild(div);
+            tr.appendChild(td);
+        });
+        tbody.appendChild(tr);
+    });
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
+}
+
+function sortTable(column) {
+    let sortedData = [...jsonData];
+    sortDirection[column] = sortDirection[column] === 'asc' ? 'desc' : 'asc';
+    sortedData.sort((a, b) => (a[column] > b[column]) ? 1 : -1);
+    if (sortDirection[column] === 'desc') sortedData.reverse();
+    populateTable(sortedData);
+}
+
+function filterTable() {
+    let filter = document.getElementById('filterField').value.split(":");
+    let filteredData = jsonData.filter(item => item[filter[0]].includes(filter[1]));
+    populateTable(filteredData);
+}
